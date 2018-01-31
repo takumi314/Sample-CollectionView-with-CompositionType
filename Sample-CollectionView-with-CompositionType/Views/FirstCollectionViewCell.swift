df7e515c58b0c7f7ff83f79ad3a42df75a881b71
takumi314 <isospin_symmetry@me.com>
2018-02-01 01:02:50 +0900

    add UICollectionViewCell and Xib file

---
+++ Sample-CollectionView-with-CompositionType/Views/FirstCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FirstCollectionViewCell.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/01.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class FirstCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        UINib(nibName: <#T##String#>, bundle: <#T##Bundle?#>)
+    }
+
+}
