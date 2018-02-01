ed51ae043860ff538014fba17716ad364ea0259c
takumi314 <isospin_symmetry@me.com>
2018-02-02 01:16:45 +0900

    update

--- Sample-CollectionView-with-CompositionType/FirstViewController.swift
+++ Sample-CollectionView-with-CompositionType/FirstViewController.swift
@@ -12,7 +12,7 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var tableView: UICollectionView!
 
-    private let dataSource = FirstCollectionViewDataSource()
+    private var dataSource = FirstCollectionViewDataSource([])
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +24,19 @@ class FirstViewController: UIViewController {
         tableView.dataSource = dataSource
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        render(["A", "B", "D", "E", "F", "G"])
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     func render(_ items: [String]) {
-        dataSource.items = items
+        dataSource.setItems(items)
         tableView.reloadData()
     }
 
