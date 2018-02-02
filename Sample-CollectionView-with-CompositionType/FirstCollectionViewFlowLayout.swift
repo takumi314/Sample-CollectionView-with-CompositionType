29e422c45af46a56f63a834a60095e8db4cb4086
takumi314 <isospin_symmetry@me.com>
2018-02-02 09:08:09 +0900

    first step to setting-up CollectionViewLayoutFlow

---
+++ Sample-CollectionView-with-CompositionType/FirstCollectionViewFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  FirstCollectionViewFlowLayout.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/02.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
