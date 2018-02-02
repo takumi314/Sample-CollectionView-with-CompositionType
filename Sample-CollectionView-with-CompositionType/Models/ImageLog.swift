498b49306016353bb6ee3d836f306d69c4c8bbed
takumi314 <isospin_symmetry@me.com>
2018-02-03 00:52:08 +0900

    define the data structure for composing a photo data

--- Sample-CollectionView-with-CompositionType/Models/ImageLog.swift
+++ Sample-CollectionView-with-CompositionType/Models/ImageLog.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct ImageLog {
+    let image: UIImage
+    let name: String
+    let date: Date
+
+    init(image: UIImage, name: String, date: Date) {
+        self.image  = image
+        self.name   = name
+        self.date   = date
+    }
+
+}
