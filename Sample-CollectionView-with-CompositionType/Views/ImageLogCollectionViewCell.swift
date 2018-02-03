d3936f4581321e0d0413a7feb3dc9bf6249cf011
takumi314 <isospin_symmetry@me.com>
2018-02-03 14:09:08 +0900

    add new file

---
+++ Sample-CollectionView-with-CompositionType/Views/ImageLogCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLogCollectionViewCell.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/03.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ImageLogCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
