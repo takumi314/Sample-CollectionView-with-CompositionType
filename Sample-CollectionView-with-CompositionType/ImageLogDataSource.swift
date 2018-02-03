d9cdd6507bf12551281536fdb2e25257daa67686
takumi314 <isospin_symmetry@me.com>
2018-02-03 10:42:04 +0900

    confirms to UICollectionViewDataSource

--- Sample-CollectionView-with-CompositionType/ImageLogDataSource.swift
+++ Sample-CollectionView-with-CompositionType/ImageLogDataSource.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ImageLogDataSource: NSObject, UICollectionViewDataSource {
+
+    private var imageLogs = [ImageLog]()
+
+    func add(_ log: ImageLog) {
+        imageLogs.append(log)
+    }
+
+    func deleteAll() {
+        imageLogs.removeAll()
+    }
+
+    func delete(at indexPath: IndexPath) {
+        imageLogs.remove(at: indexPath.row)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageLogs.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+
+        return cell
+    }
+
+}
