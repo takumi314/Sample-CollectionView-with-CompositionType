cad6c317705cf3da42d9fd2e4367c82533c2bb93
takumi314 <isospin_symmetry@me.com>
2018-02-03 17:35:10 +0900

    set up configuration with the calculation

--- Sample-CollectionView-with-CompositionType/Compositions/FlowLayouts/FirstCollectionViewFlowLayout.swift
+++ Sample-CollectionView-with-CompositionType/Compositions/FlowLayouts/FirstCollectionViewFlowLayout.swift
@@ -8,7 +8,33 @@
 
 import UIKit
 
+let numberOfColumn: CGFloat = 2
+let screenWidth = UIScreen.main.bounds.width
+let minimumSpacing: CGFloat = 20
+
 class FirstCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
+    // MARK: - Initializer
+
+    override init() {
+        super.init()
+
+        let itemOfWidth = (screenWidth - 4 * minimumSpacing) / numberOfColumn
+        let itemSizs = CGSize(width: itemOfWidth, height: itemOfWidth)
+
+        self.itemSize = itemSizs
+        self.minimumLineSpacing = itemSizs.height * 0.25
+        self.minimumInteritemSpacing = minimumSpacing
+        self.scrollDirection = .vertical
+
+        let horizontalInset = CGFloat((screenWidth - numberOfColumn * itemOfWidth) * 0.25)
+        let verticalInset   = CGFloat((screenWidth - itemSizs.height) * 0.5)
+
+        self.sectionInset = UIEdgeInsetsMake(verticalInset, horizontalInset, verticalInset, horizontalInset)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
 }
