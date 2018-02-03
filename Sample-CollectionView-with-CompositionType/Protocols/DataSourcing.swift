c04dfd5b82ac9ce2f0b2f7d836610760cd399d01
takumi314 <isospin_symmetry@me.com>
2018-02-03 11:10:52 +0900

    add new protocol for data source

---
+++ Sample-CollectionView-with-CompositionType/Protocols/DataSourcing.swift
@@ -0,0 +1,13 @@
+//
+//  DataSourcing.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/03.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+protocol DataSourcing {
+    
+}
