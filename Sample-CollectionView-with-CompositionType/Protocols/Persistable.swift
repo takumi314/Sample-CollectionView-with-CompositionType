ab04c89544184666c8facc23b23a3108d39c230e
takumi314 <isospin_symmetry@me.com>
2018-02-05 02:24:51 +0900

    add UserDefault with Persistable

--- Sample-CollectionView-with-CompositionType/Protocols/Persistable.swift
+++ Sample-CollectionView-with-CompositionType/Protocols/Persistable.swift
@@ -13,3 +13,35 @@ protocol Persistable {
     func load<T>() -> T?
 }
 
+// MARK: - UserDefaults
+
+extension Persistable where Self: UserDefaults {
+    // Only declares for UserDefault
+}
+
+extension UserDefaults: Persistable {
+    func save<T>(_ data: T) {
+        self.set(data, forKey: "IMAGE_LOG")
+    }
+    func load<T>() -> T? {
+        return object(forKey: "IMAGE_LOG") as? T
+    }
+
+}
+
+struct DataStore<U> {
+    typealias T = U
+    let repository: Persistable
+
+    init(_ repository: Persistable) {
+        self.repository = repository
+    }
+
+    func save(_ data: U) {
+        repository.save(data)
+    }
+
+    func load() -> U? {
+        return repository.load()
+    }
+}
