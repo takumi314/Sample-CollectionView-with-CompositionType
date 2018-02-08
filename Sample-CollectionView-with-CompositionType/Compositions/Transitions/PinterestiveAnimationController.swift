21291d3bf8971bf4bb18ce743b06565d5f072fd8
takumi314 <isospin_symmetry@me.com>
2018-02-09 01:46:49 +0900

    add new file

---
+++ Sample-CollectionView-with-CompositionType/Compositions/Transitions/PinterestiveAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  PinterestiveAnimationController.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/09.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
