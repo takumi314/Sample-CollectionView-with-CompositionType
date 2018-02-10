d16748f995ea3fe2953f721eab0daa7152c6cdb0
takumi314 <isospin_symmetry@me.com>
2018-02-10 15:09:56 +0900

    add new file to `presented` ViewController

---
+++ Sample-CollectionView-with-CompositionType/ImageDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageDetailViewController.swift
+//  Sample-CollectionView-with-CompositionType
+//
+//  Created by NishiokaKohei on 2018/02/10.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
