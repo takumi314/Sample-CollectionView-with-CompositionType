454af0de1c7fdb123392fb878ef5d50b19953b31
takumi314 <isospin_symmetry@me.com>
2018-02-10 15:16:34 +0900

    connect to IBOutlet

--- Sample-CollectionView-with-CompositionType/ImageDetailViewController.swift
+++ Sample-CollectionView-with-CompositionType/ImageDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ImageDetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
